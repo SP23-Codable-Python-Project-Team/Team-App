fb2de91c4a7448970acdd243f097e2232a9bce7a
Ky-Lee-375 <kl35@illinois.edu>
2023-02-24 17:17:28 -0600

    Made a new version

---
+++ WhatIAteToday.Ver1/WhatIAteToday.Ver1/WhatIAteToday_Ver1App.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIAteToday_Ver1App.swift
+//  WhatIAteToday.Ver1
+//
+//  Created by Kyungyeon Lee on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatIAteToday_Ver1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
